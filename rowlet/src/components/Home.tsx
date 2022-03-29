8691969e20f5106219980b6e0e4e2c9415dd6988
clucidojr123 <clucidojr123@gmail.com>
2022-03-29 10:18:01 -0400

    switch base repo

---
+++ rowlet/src/components/Home.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Home() {
+  return (
+    <>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Home Page
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </>
+  );
+}
+
+export default Home;
