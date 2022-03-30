5c3b4595584bc715a0f4085cc2110797605ea561
clucidojr123 <clucidojr123@gmail.com>
2022-03-30 15:04:01 -0400

    add luigi sharedb server

---
+++ luigi/index.ts
@@ -0,0 +1,44 @@
+import http from "http";
+import cors from "cors";
+import express from "express";
+import ShareDB from "sharedb";
+import WebSocket from "ws";
+// @ts-ignore -- no type declarations available at the moment
+import WebSocketJSONStream from "@teamwork/websocket-json-stream";
+// @ts-ignore -- no type declarations available at the moment
+import richText from "rich-text";
+import Delta from "quill-delta";
+
+const PORT = process.env.PORT || 5001;
+
+// Register Quill-Delta OT Type with ShareDB
+ShareDB.types.register(richText.type);
+
+async function main() {
+    // Express Web Server
+    const app = express();
+    const server = http.createServer(app);
+
+    // Initialize ShareDB
+    const share = new ShareDB();
+
+    // Connect incoming WebSocket connections to ShareDB
+    const wss = new WebSocket.Server({ server: server });
+    wss.on("connection", (ws) => {
+        const stream = new WebSocketJSONStream(ws);
+        share.listen(stream);
+    });
+
+    app.use(cors());
+
+    // Sanity Check
+    app.get("/", (req, res, next) => {
+        res.send("gigabossofswag-luigi");
+        return next();
+    });
+
+    server.listen(PORT);
+    console.log(`🚀 Luigi (ShareDB Server) now listening on port ${PORT}`);
+}
+
+main().catch((err) => console.log(err));
